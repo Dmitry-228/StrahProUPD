59d481fc78e8954c30e6f76d7b9936dff06ae6c6
stas0022 <Stas44710@gmail.com>
2021-05-12 18:53:46 +0300

    Initial commit

---
+++ app/src/main/java/com/example/strahpro/ui/start/StartFragment.java
@@ -0,0 +1,35 @@
+package com.example.strahpro.ui.start;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.strahpro.R;
+import com.example.strahpro.ui.osago.HomeViewModel;
+
+
+public class StartFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_start, container, false);
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return root;
+    }
+}
